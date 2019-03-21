297fae503fd4d8f2974fa9e10c1e6d9e0dc2cd58
AndreasWintherMoen <andreas@winthermoen.no>
2019-03-21 12:41:00 +0100

    Refactor Songs.vue into sub-components and make song search display

---
+++ client/src/components/Songs/SongsPanel.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <panel title="Songs">
+      <v-btn
+        slot="action"
+        to="songs/create"
+        class="cyan accent-3"
+        light
+        small
+        absolute
+        right
+        middle
+        fab>
+        <v-icon>add</v-icon>
+      </v-btn>
+      <div v-for="song in songs" v-bind:key="song.id" class="song">
+        <v-layout>
+          <v-flex xs6>
+            <div class="song-title">
+              {{song.title}}
+            </div>
+            <div class="song-artist">
+              {{song.artist}}
+            </div>
+            <div class="song-genre">
+              {{song.genre}}
+            </div>
+            <v-btn
+              dark
+              class="cyan"
+              @click="navigateTo({
+                name: 'song',
+                params: {songId: song.id}})">
+              View
+            </v-btn>
+          </v-flex>
+          <v-flex xs6>
+            <img class="album-image" v-bind:src="song.albumImageUrl">
+          </v-flex>
+        </v-layout>
+      </div>
+    </panel>
+  </div>
+</template>
+
+<script>
+import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
+  components: {
+    Panel
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
+  async mounted () {
+    // do a request to the backend for all the songs
+    this.songs = (await SongsService.getSongs()).data
+  }
+}
+</script>
+
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 30px;
+}
+.song-artist {
+  font-size: 24px;
+}
+.song-genre {
+  font-size: 18px;
+}
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
---
+++ client/src/components/Songs/SongsSearchPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <panel title="Search"></panel>
+  </div>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+</style>
