bb5986ae535214bac8ab319b34d0478daf7bc5de
AndreasWintherMoen <andreas@winthermoen.no>
2019-04-12 19:41:00 +0200

    Finish bookmarks

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -19,6 +19,20 @@
             params: {songId: song.id}})">
           Edit
         </v-btn>
+        <v-btn
+          v-if="isUserLoggedIn && !bookmark"
+          flat
+          @click="setAsBookmark">
+          <v-icon>outlined_flag</v-icon>
+          Bookmark
+        </v-btn>
+        <v-btn
+          v-if="isUserLoggedIn && bookmark"
+          flat
+          @click="unsetAsBookmark">
+          <v-icon>flag</v-icon>
+          Unbookmark
+        </v-btn>
       </v-flex>
       <v-flex xs6>
         <img class="album-image" v-bind:src="song.albumImageUrl">
@@ -31,16 +45,68 @@
 
 <script>
 import Panel from '@/components/Panel'
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
+
 export default {
   props: [
     'song'
   ],
+  data () {
+    return {
+      bookmark: null
+    }
+  },
   components: {
     Panel
   },
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    async setAsBookmark () {
+      console.log('set as bookmark')
+      try {
+        const newBookmark = {
+          songId: this.song.id,
+          userId: this.$store.state.user.id
+        }
+        this.bookmark = (await BookmarksService.post(newBookmark)).data
+        console.log(!!this.bookmark)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async unsetAsBookmark () {
+      console.log('unset as bookmark')
+      try {
+        await BookmarksService.delete(this.bookmark.id)
+        this.bookmark = null
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn'
+    ])
+  },
+  watch: {
+    async song (value) {
+      if (!this.isUserLoggedIn) {
+        return
+      }
+      try {
+        const query = {
+          songId: this.song.id,
+          userId: this.$store.state.user.id
+        }
+        console.log('updating bookmark')
+        this.bookmark = (await BookmarksService.index(query)).data
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -1,4 +1,4 @@
-const {Bookmark} = require('../models')
+const {Bookmark, Song, User} = require('../models')
 
 module.exports = {
   async index(req, res) {
@@ -14,6 +14,48 @@ module.exports = {
     } catch (err) {
       res.status(204).send({
         error: 'An error has occured trying to get the bookmark'
+      })
+    }
+  },
+  async post(req, res) {
+    try {
+      const {songId, userId} = req.body
+      const bookmark = await Bookmark.findOne({
+        where: {
+          SongId: songId,
+          UserId: userId
+        }
+      })
+      if (bookmark) {
+        console.log('this song is already bookmarked')
+        return res.status(400).send({
+          error: 'This song is already bookmarked'
+        })
+      }
+      const newBookmark = await Bookmark.create(req.body)
+      const song = await Song.findById(songId)
+      const user = await User.findById(userId)
+      await newBookmark.setUser(user)
+      await newBookmark.setSong(song)
+      res.send(newBookmark)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to create the bookmark'
+      })
+    }
+  },
+  async delete(req, res) {
+    try {
+      const {bookmarkId} = req.params
+      console.log('Deleting bookmark', bookmarkId)
+      const bookmark = await Bookmark.findByPk(bookmarkId)
+      console.log('Found bookmark', bookmark)
+      await bookmark.destroy()
+      console.log('Removed bookmark')
+      res.send(bookmark)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to delete the bookmark'
       })
     }
   }
